563b33d29dde7257faeab14e6dfb9ca73b595c66
QipengZhou <xust07@gmail.com>
2026-04-19 18:34:20 +0800

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.py
@@ -0,0 +1,21 @@
+from functools import cache
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s or s[0] == '0':
+            return 0
+
+        @cache
+        def dfs(i: int) -> int:
+            if i == len(s):
+                return 1
+
+            if s[i] == '0':
+                return 0
+            res = dfs(i+1)
+
+            if i + 1 < len(s):
+                if s[i] == '1' or (s[i] == '2' and s[i+1] in "0123456"):
+                    res += dfs(i+2)
+            return res
+        return dfs(0)
