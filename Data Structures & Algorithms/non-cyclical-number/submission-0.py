986870a349790660591543e62958edb691bf373f
QipengZhou <xust07@gmail.com>
2026-05-10 07:07:22 +0800

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n != 1:
+            if n in seen:
+                break
+            seen.add(n)
+            n = sum(int(i)**2 for i in str(n))
+        return n == 1
