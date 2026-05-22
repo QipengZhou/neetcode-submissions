b750e677c383574d857929b3963b0ade8102f32d
QipengZhou <xust07@gmail.com>
2026-05-22 21:52:40 +0800

    Add: reconstruct-flight-path - submission-2

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-2.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = defaultdict(list)
+        for src, dst in sorted(tickets, reverse=True):
+            graph[src].append(dst)
+        ans = []
+        def dfs(airport):
+            while graph[airport]:
+                next_airport = graph[airport].pop()
+                dfs(next_airport)
+            ans.append(airport)
+        dfs("JFK")
+        return ans[::-1]
